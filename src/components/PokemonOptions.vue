99f8290131d3bfdf32c5e89967d386a722bee2fb
Seb4sti4n27 <27sebastian4@gmail.com>
2023-10-18 18:32:33 -0500

    Poke-api Creamos los components

---
+++ src/components/PokemonOptions.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="options-container">
+    <ul>
+      <!--Todo :Para los components es improtante usar un :key como identificador -->
+      <!--Todo: El evento @click  llamamos el $emite un selection y tambien mandamos el id al componente padre-->
+      
+      <li v-for="pokemn in pokemons" 
+          :key="pokemn.id"
+          
+          @click="$emit('selection', pokemn.id)"
+          
+          >
+          {{ pokemn.name }}
+      </li>
+      
+    </ul>
+  </div>
+
+</template>
+
+<script>
+export default {
+  props:{
+      pokemons:{
+        type:Array,
+        required:true
+        
+        
+      }
+  }
+
+}
+</script>
+
+<style Scoped>
+  ul {
+    /*Con esto quitamos los . de cada elemento li*/
+    list-style-type: none;
+    padding-right: 35px;
+}
+li {
+    background-color: white;
+    border-radius: 5px;
+    border: 1px solid rgba(24, 23, 23, 0.2);
+    cursor: pointer;
+    margin-bottom: 10px;
+    width: 250px;
+}
+
+li:hover {
+    background-color: rgba(0, 0, 0, 0.05);
+}
+
+.options-container {
+    display: flex;
+    justify-content: center;
+    
+}
+
+
+</style>
---
+++ src/components/PokemonPicture.vue
@@ -0,0 +1,76 @@
+<template>
+  
+    <div class="pokemon-container">
+      <img :src="imgSrc" alt="pokemon" class="hidden-pokemon">
+      <img v-if="ShowPokemon" :src="imgSrc" alt="pokemon" class="fade-in" id="hidden-pokemon">
+     
+    </div>
+    
+  
+</template>
+
+<script>
+
+export default {
+    props:{
+      pokemonId:{
+        type:Number,
+        required:true
+
+      },
+      ShowPokemon:{
+        type:Boolean,
+        default:false,
+        required:true
+      }
+    },
+    
+    computed: {
+      imgSrc(){
+        return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${this.pokemonId}.svg`
+      }
+
+    }
+
+
+}
+</script>
+
+<style scoped>
+
+.pokemon-container {
+    
+    width: auto;
+    height: 230px;
+    overflow: hidden;
+    position: relative;
+        
+}
+img {
+    height: 200px;
+    position: absolute;
+    /*Aqui quitamos los lados */ 
+    left: 0;
+    right: 0;
+    /*Con esto de arriba abajo le ponemos 0 y auto para centrar el img*/
+    margin: 0 auto;
+    /*Codigo para quitar opciones del navegador */
+    user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    -webkit-user-drag: none;
+    -webkit-user-select: none;
+     width: 100%; /* esto hará que la imagen tenga el ancho del contenedor */
+     display: block
+    
+    
+}
+
+.hidden-pokemon {
+    filter: brightness(0);
+    
+}
+
+
+</style >
+
