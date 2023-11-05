bba4124bbef9eca25b275e415e4f8175be504d35
Seb4sti4n27 <27sebastian4@gmail.com>
2023-11-05 03:29:39 -0500

    Pokemon-Api- terminado el game over

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="container">
-
-    <h1 v-if="!pokem"><span class="loader"></span></h1>
+  <div class="container" v-if="!gameover">
+    <div class="contet">  
+      <h1 v-if="!pokem"><span class="loader"></span></h1>
   
-    <div v-else class="conte">
+      <div v-else class="conte">
       <h1>¿Quien es este Pokemon?</h1>
       
       <!-- TODO : Img-->
@@ -20,17 +20,22 @@
 
 
       <!--Todo: Aqui podemos usar el template en caso que no queramos el div -->
-
+      
       <div v-if="showasnwers">
         <h3 class="fade-in">{{ message }}</h3>
         <button
-          @click="newgame"  
-          >New Game </button>
+          @click="newgame" v-if="fallas >= 0 && fallas<10"  
+          >New Game</button>
+      </div>
+
+      <div v-if="mostrar">
+        <button @click="resetGame" >Restart</button>
       </div>
     
     </div>
+    
 
-    <div class="table">
+    <div  class="table" v-if="isLoading">
       <table border="1">
         <thead>
         <tr>
@@ -42,19 +47,24 @@
 
         <tbody>
                 <tr>
-                  <td>num</td>
-                  <td>num</td>
+                  <td>{{correctas}}</td>
+                  <td>{{fallas}}</td>
                 </tr>
         </tbody>
-    </table>
-      
+      </table>
+    </div>
+    </div>
   </div>
 
+
+  <div v-if="gameover">
+    <div class="game-over-overlay">
+      <p class="game-over-text">GAME OVER</p>
+    <button @click="resetGame">Restart</button>
+    </div>
   </div>
   
   
-  
-
 </template>
 
 <script>
@@ -77,14 +87,21 @@ export default {
           pokem:null,
           showpokemn:false,
           message:null,
-          showasnwers:false
+          showasnwers:false,
+          isLoading: true,
+          fallas:0,
+          correctas:0,
+          mostrar:false,
+          gameover:false
       }
     },
     /* Creamos Un metodo donde tendre una funcion asyncrona en la cual espera una promesa del getpokemonoptions y
     Almacenamos en el array pokemonArr */
     methods:{
          async mixPokemonArray(){
+          this.isLoading = false;
           this.pokemonArr =  await getPokemonOptions()
+          this.isLoading = true;
          
           /*Aqui tenemos el objeto reactivo   */
           //console.log(this.pokemonArr);
@@ -94,25 +111,33 @@ export default {
 
           /*Con esto mostramos la data del iindice generado*/
           this.pokem = this.pokemonArr[randon]
+          
 
           console.log(this.pokem)
           
                  
         },
 
-        traerpokem(pokemonId){
+        traerpokem(pokemonId) {
 
           const pokems = this.pokemonArr.find(p => p.id == pokemonId)
-          const pokn=this.pokem
-          if (pokems.id !== pokn.id){
-            this.message=`Fallaste , es el pokemon ${pokn.name}`;
+          const pokn = this.pokem
+          if (pokems.id !== pokn.id) {
+            this.message = `Fallaste , es el pokemon ${pokn.name}`;
+            this.fallas++
+            if (this.fallas >= 10) {
+              this.message = "GAME OVER";
+              this.mostrar= true
+              this.gameover = true;
+            }
           }
-          else{
-            this.message=`Correcto `
+          else {
+            this.message = `Correcto `
+            this.correctas++
           }
-          
-          this.showpokemn=true
-          this.showasnwers=true
+
+          this.showpokemn = true
+          this.showasnwers = true
         },
         newgame(){
         
@@ -121,9 +146,18 @@ export default {
           this.pokem=null
           this.mixPokemonArray()
 
-        }
-        
+        },
+        resetGame() {
+      this.showpokemn = false;
+      this.showasnwers = false;
+      this.pokem = null;
+      this.fallas = 0;
+      this.correctas=0
+      this.mostrar=false
+      this.gameover=false // Reinicia el contador de fallas
+      this.mixPokemonArray();
 
+      }
     },
     
     /*A quie usamos ciclo de vida de los components*/
@@ -153,10 +187,10 @@ button{
   margin-bottom: 15px;
 }
 .table{
-  margin-right: 20%;
-  margin-bottom: 25%;
+  margin: 0 auto;
   justify-content: space-around;
   align-items: center;
+  padding: 10px;
   
 }
 
@@ -172,6 +206,13 @@ table{
   justify-content: center;
   align-items: center;
   height: 90vh;
+}
+.contet{
+  display: flex;
+  justify-content: center;
+  width: auto;
+  
+
 }
 .table th ,td{
   color: black;
@@ -191,6 +232,56 @@ table{
   box-sizing: border-box;
   animation: rotation 1s linear infinite;
 }
+.game-over-text {
+    font-size: 24px;
+    font-weight: bold;
+    text-align: center;
+    margin-bottom: 10px;
+    text-align: center;
+  }
+  .game-over-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background: rgba(0, 0, 0, 0.7);
+    z-index: 999;
+  }
+  .game-over-text {
+    font-size: 48px;
+    font-weight: bold;
+    text-align: center;
+    color: white;
+    margin-bottom: 20px;
+    animation: fadeIn 1s ease-in-out; 
+  }
+  .game-over-text button {
+    font-size: 24px;
+    background-color: #3498db;
+    color: white;
+    border: none;
+    padding: 10px 20px;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: background-color 0.3s;
+  }
+  .game-over-text button:hover {
+    background-color: #2980b9;
+  }
+
+  @keyframes fadeIn {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
 
 @keyframes rotation {
   0% {
@@ -200,6 +291,18 @@ table{
     transform: rotate(360deg);
   }
 } 
+@media screen and (max-width: 510px) {
+  .table{
+    width: 45%;
+    
+  }
+  .contet{
+    margin-top: 70px;
+    flex-direction: column;
+    
+  }
+  
+}
 
 
 </style>
