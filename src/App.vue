e05852fd702c62bbfacd9b8e7a0b3ca85132471d
Seb4sti4n27 <27sebastian4@gmail.com>
2023-12-10 02:15:36 -0500

    Finish-project

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <!-- <img class="fade-in" alt="Vue logo" src="./assets/logo.png">-->
   <!-- <HelloWorld msg="Welcome to Your Vue.js App"/>-->
-  <PokemonPageVue/>
+  
+  <PokemonPageVue />
 </template>
 
 <script>
@@ -23,9 +24,8 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #b9c9da;
-  margin-top: 0px;
   width: 100%; 
-  height: 100vh; background: rgb(100, 99, 129); background: -moz-linear-gradient(left, rgb(127, 125, 158) 0%, rgb(131, 167, 160) 100%); background: -webkit-gradient(left top, right top, color-stop(0%, rgb(116, 113, 172)), color-stop(100%, rgb(143, 168, 163))); background: -webkit-linear-gradient(left, rgb(131, 130, 151) 0%, rgb(112, 133, 129) 100%); background: -o-linear-gradient(left, rgba(25,22,84,1) 0%, rgb(119, 172, 161) 100%); background: -ms-linear-gradient(left, rgba(25,22,84,1) 0%, rgb(156, 172, 169) 100%); background: linear-gradient(to right, rgb(165, 165, 177) 0%, rgb(126, 146, 142) 100%); 
+  height: 100vh; background: rgb(100, 99, 129); background: -moz-linear-gradient(left, rgb(127, 125, 158) 0%, rgb(131, 167, 160) 100%); background: -webkit-gradient(left top, right top, color-stop(0%, rgb(116, 113, 172)), color-stop(100%, rgb(143, 168, 163))); background: -webkit-linear-gradient(left, rgb(131, 130, 151) 0%, rgb(112, 133, 129) 100%); background: -o-linear-gradient(left, rgba(25,22,84,1) 0%, rgb(119, 172, 161) 100%); background: -ms-linear-gradient(left, rgba(25,22,84,1) 0%, rgb(156, 172, 169) 100%); background: linear-gradient(to right, rgb(0, 0, 0) 0%, rgb(100, 99, 100) 100%); 
  
   
   
@@ -34,5 +34,34 @@ export default {
     #app{
       height: 100%;
     }
+}
+
+</style>
+
+<style scoped>
+/* todo:  Anconrtacs */
+
+
+.aconrts h1{
+  font-size: 2.5em;
+  font-weight: 700;
+  color: #b9c9da;
+  margin: 0;
+  padding: 0;
+  text-align: center;
+  
+
+}
+.aconrts{
+  display: flex;
+  justify-content: center;
+  width: auto;
+}
+.aconrts li{
+  list-style: none;
+  width: auto;
+  font-size: 30px;
+  margin-top: 20px;
+
 }
 </style>
--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -20,7 +20,7 @@
         <!--Todo: Aqui podemos usar el template en caso que no queramos el div -->
 
         <div v-if="showasnwers">
-          <h3 class="fade-in">{{ message }}</h3>
+          <h3 class="fade-in" style="color: black;">{{ message }}</h3>
           <button @click="newgame" v-if="fallas >= 0 && fallas < 10" class="newbutton">New Game</button>
         </div>
 
@@ -35,18 +35,36 @@
       <div v-if="pokem" class="pokacertado">
         <!-- <h1>Pokemons acertados</h1> -->
         <!-- ... (otro contenido) -->
-        <PokemonesAsertados :pokems="pokemonesAcertados" :correctas="correctas"  />
+        <PokemonesAsertados :pokems="pokemonesAcertados" :correctas="correctas" />
       </div>
 
+      <div v-if="pokem" class="aconrts">
+        <li style="color: rgb(238, 235, 235); width: 20px; margin-left: 15px; margin-right: 20px; ">
+          <a class="nav-link py-2 px-0 px-lg-2" href="https://github.com/Seb4sti4n27" target="_blank" rel="noopener"
+            style="width: auto; color:  rgb(95, 175, 175);">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" class="navbar-nav-svg" viewBox="0 0 512 499.36"
+              role="img">
+              <title>GitHub</title>
+              <path fill="currentColor" fill-rule="evenodd"
+                d="M256 0C114.64 0 0 114.61 0 256c0 113.09 73.34 209 175.08 242.9 12.8 2.35 17.47-5.56 17.47-12.34 0-6.08-.22-22.18-.35-43.54-71.2 15.49-86.2-34.34-86.2-34.34-11.64-29.57-28.42-37.45-28.42-37.45-23.27-15.84 1.73-15.55 1.73-15.55 25.69 1.81 39.21 26.38 39.21 26.38 22.84 39.12 59.92 27.82 74.5 21.27 2.33-16.54 8.94-27.82 16.25-34.22-56.84-6.43-116.6-28.43-116.6-126.49 0-27.95 10-50.8 26.35-68.69-2.63-6.48-11.42-32.5 2.51-67.75 0 0 21.49-6.88 70.4 26.24a242.65 242.65 0 0 1 128.18 0c48.87-33.13 70.33-26.24 70.33-26.24 14 35.25 5.18 61.27 2.55 67.75 16.41 17.9 26.31 40.75 26.31 68.69 0 98.35-59.85 120-116.88 126.32 9.19 7.9 17.38 23.53 17.38 47.41 0 34.22-.31 61.83-.31 70.23 0 6.85 4.61 14.81 17.6 12.31C438.72 464.97 512 369.08 512 256.02 512 114.62 397.37 0 256 0z">
+              </path>
+            </svg>
+          </a>
+        </li>
+        <p style="position: absolute; margin-top: 25px; font-size: 15px; margin-right: 5px;">Github</p>
+        <!-- </a> -->
+        <!-- <li class="nav-item col-6 col-lg-auto"></li> -->
+      </div>
     </div>
   </div>
 
 
-  <div v-if="gameover">
+  <div v-if="gameover ">
     <div class="game-over-overlay">
-      <p class="game-over-text">GAME OVER</p>
+      <p class="game-over-text">GAME OVER 😔</p>
       <h1>Haz Acertado {{ correctas }} Pokemons</h1>
-      <button @click="resetGame" class="restart">Restart</button>
+      <h2 v-if="correctas>=5">Eres Fanatico de Pokemons 😄 </h2>
+      <button @click="resetGame" class="restart"> Restart</button>
     </div>
   </div>
 </template>
@@ -109,9 +127,9 @@ export default {
     traerpokem(pokemonId) {
       const pokems = this.pokemonArr.find(p => p.id == pokemonId)
       const pokn = this.pokem
-      
+
       if (pokems.id !== pokn.id) {
-        this.message = `oh!! you failed try again 😞`;
+        this.message = ` ❌ oh!! you failed try again 😞`;
         this.fallas++
         if (this.fallas >= 10) {
           this.message = "GAME OVER";
@@ -120,7 +138,7 @@ export default {
         }
       }
       else {
-        this.message = `Yeah !!! it's right 😎 `
+        this.message = ` ✅ Yeah !!! it's right 😎 `
         this.correctas++
         // Agregar el Pokémon acertado al array
         this.pokemonesAcertados.push(pokn);
@@ -377,6 +395,7 @@ table {
     flex-direction: column;
 
   }
+
   .container {
     height: 100%;
     width: 100%;
@@ -390,5 +409,57 @@ table {
     height: 50%;
     margin-left: 0px;
   }
+  .aconrts{
+    margin-top: 20px;
+    margin-bottom: 20px;
+    margin-right: 54px;
+  }
+  .aconrts p{
+    color: #e2d9d9;
+  }
+}
+
+/* todo:  Anconrtacs */
+.aconrts h1 {
+  font-size: 2.5em;
+  font-weight: 700;
+  color: #b9c9da;
+  margin: 0;
+  padding: 0;
+  text-align: center;
+
+}
+
+.aconrts {
+  margin-left: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: start;
+  width: auto;
+}
+
+.aconrts li {
+  list-style: none;
+  width: 20px;
+  height: 23px;
+  font-size: 23px;
+  border-radius: 20px;
+}
+
+.aconrts li,
+p {
+  margin-top: 0px;
+
+}
+
+.aconrts p {
+  color: #ffffff;
+  font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+
+}
+
+.aconrts p:hover {
+  color: #b4b3b3;
+
 
 }</style>
